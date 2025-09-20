4f21cd5742b284edd1a97fccc72e50b266570a8d
1LunarVeil <3415320767@qq.com>
2025-09-20 16:32:05 +0800

    Site updated: 2025-09-20 16:32:05

---
+++ js/third-party/analytics/matomo.js
@@ -0,0 +1,3 @@
+// build time:Sat Sep 20 2025 15:15:22 GMT+0800 (中国标准时间)
+if(CONFIG.matomo.enable){window._paq=window._paq||[];const e=window._paq;e.push(["trackPageView"]);e.push(["enableLinkTracking"]);const t=CONFIG.matomo.server_url;e.push(["setTrackerUrl",t+"matomo.php"]);e.push(["setSiteId",CONFIG.matomo.site_id]);const o=document;const s=o.createElement("script");const a=o.getElementsByTagName("script")[0];s.async=true;s.src=t+"matomo.js";a.parentNode.insertBefore(s,a)}
+//rebuild by hrmmi 
